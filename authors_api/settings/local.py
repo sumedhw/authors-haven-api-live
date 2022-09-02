c455df96b2ce674548e92fd187c25c64c6164898
Sumedh Waghmare <sumedhw71@gmail.com>
2022-09-02 09:55:44 +0530

    project setting structure updated

---
+++ authors_api/settings/local.py
@@ -0,0 +1,11 @@
+from .base import *
+from .base import env
+
+
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = env("DJANGO_SECRET_KEY", default="django-insecure-)#nxzzzt)wk)b+cu4bd4erx$&1t4e9z_@!p=xneo#t13%-(ak%")
+
+ALLOWED_HOSTS = [ "localhost", "0.0.0.0", "127.0.0.1" ]
