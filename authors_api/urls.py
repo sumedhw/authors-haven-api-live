02556fb5fbc2b82d150d5c6ac32c4c8cf46573ef
Sumedh Waghmare <sumedhw71@gmail.com>
2023-02-18 14:14:53 +0530

    user authentication functionality

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -23,6 +23,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path(settings.ADMIN_URL, admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
 ]
 
 
